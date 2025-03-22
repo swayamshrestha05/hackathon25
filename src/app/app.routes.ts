747712f13b6b03bd2c23600add71256e261595c2
swayamshrestha05 <ss89@calvin.edu>
2025-03-22 13:56:03 -0400

    css and route not working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecordComponent } from './components/record/record.component';
+import { ResultsComponent } from './components/results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: RecordComponent},
+    { path: 'results', component: ResultsComponent},
+    { path: '**', redirectTo: ''},
+];
