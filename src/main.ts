a578dcba0eb8e64481619c87a5c8057e6d6eabe7
Aishwarya Joshi <aishwaryajoshi@dhcp82-91.calvin.edu>
2025-03-22 17:11:12 -0400

    css

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(), // Configure HttpClient here
-  ],
-}).catch((err) => console.error(err));
